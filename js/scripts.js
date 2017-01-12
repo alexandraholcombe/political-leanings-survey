f9f07a2c12635465c4390b7c2a0f7cc58f9c9bc6
Alexandra Holcombe and Grinil Khanna <alexandra@>
2017-01-12 11:42:24 -0800

    Add JS branching and pre-alert testing to survey results

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $("#survey form").submit(function(event) {
   var wallSelect = parseInt($("#wall").val());
   var climateSelect = parseInt($("#climate").val());
@@ -6,6 +7,22 @@ $(document).ready(function() {
   var gunSelect = parseInt($("#gun").val());
   var marriageSelect = parseInt($("#marriage").val());
 
+  var surveyValue = (wallSelect + climateSelect + healthSelect + gunSelect + marriageSelect);
+
+  if (surveyValue <= 7) {
+    $("#very-liberal").show();
+    alert("Very liberal");
+  } else if (8 <= surveyValue <= 10) {
+    $("#liberal").show();
+    alert("liberal");
+  } else if (11 <= surveyValue <= 13) {
+    $("#non-partisan").show();
+  } else if (14 <= surveyValue <= 20) {
+    $("#conservative").show();
+  } else {
+    $("#very-conservative").show();
+    alert("Very conservative");
+  };
 
   event.preventDefault();
   });
